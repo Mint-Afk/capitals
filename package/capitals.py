1841955c343df2a7841b637ef453e12f721cfb7d
Francesco Pedrini <francescopedrini@MacBook-Pro-di-Francesco-2.local>
2019-11-22 17:42:11 +0100

    update capital.py

--- package/capitals.py
+++ package/capitals.py
@@ -1,3 +1,5 @@
+''' This is a dictionary with states as keys and capitals as values. This is used by check_state and check_capital functions  '''
+
 list_of_capitals = {'Aland Islands':'Mariehamn',
 'Albania':'Tirana',
 'Andorra':'Andorra la Vella',
@@ -57,11 +59,15 @@ list_of_capitals = {'Aland Islands':'Mariehamn',
 'United Kingdom':'London',
 'Vatican City':'Vatican City'}
 
+''' 1st function: check if the input is a valid state in the capitals list, and gives in return the capital associated '''
+
 def check_capital(state_name):
     if state_name in list_of_capitals:
         print("The capital of {} is {}".format(state_name, list_of_capitals[state_name]))
     else:
         print("Sorry, {} does not seem to be an European state".format(state_name))
+
+''' 2st function: check if the input is a valid capital in the capitals list, and gives in return the state associate '''
 
 def check_state(capital_name):
     for state, capital in list_of_capitals.items():
