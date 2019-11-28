6b1d19688e698be232c47f6bbfc4a1c6c3136b12
Matteo Carniel <matteocarniel@MacBook-Pro-di-Matteo-4.local>
2019-11-28 17:14:04 +0100

    update main

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 #! /usr/bin/env python3
 
-''' Import capitals module from the python package, recalling the function check_capital '''
+""" Import capitals module from the python package,
+	 recalling the function check_capital """
 
 import city_check.capitals as cpt
+import sys
 
-''' Perform test on capitals functions '''
 
-
-cpt.check_capital("Germany")
-cpt.check_capital("Honduras")
-cpt.check_capital("Rome")
-cpt.check_capital("Tokyo")
-
+if __name__ == "__main__":
+    if len(sys.argv) > 1:  # if sys has been given an argument through the shell by user, its gonna call back to check capital function from the module and gave back the results
+        if sys.argv[1] == 'check':  # perform test on capitals functions
+            cpt.check_capital("Germany")
+            cpt.check_capital("Honduras")
+            cpt.check_capital("Rome")
+            cpt.check_capital("Tokyo")
+        else:
+            for arg in range(1,len(sys.argv)):
+                cpt.check_capital(sys.argv[arg])
+    else:
+        print("pls insert any input through shell")
+        exit()
