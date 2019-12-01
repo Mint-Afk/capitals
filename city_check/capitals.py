a7a3e85164318826cfdb8bb61b2c93fa2c07c028
Your Name <giorgia.fanton@student.h-farm.com>
2019-12-01 11:38:43 +0100

    update capitals

--- city_check/capitals.py
+++ city_check/capitals.py
@@ -1,5 +1,5 @@
 
-''' This is a dictionary with states as keys and capitals as values. This is used by check_capital function. '''
+''' This is a Python dictionary with states as keys and their related capitals as values. This dictonary is used by check_capital function. '''
 
 
 list_of_capitals = {'Aland Islands': 'Mariehamn',
@@ -62,17 +62,17 @@ list_of_capitals = {'Aland Islands': 'Mariehamn',
                     'Vatican City': 'Vatican City'}
 
 ''' The first part of the function checks if the input inserted is contained in the capitals list as a valid state and returns a sentence with the capital associated.
-    The second part checkes if the inputer inserted is contained in the capital list as a valid capital and returns a sentence with the state associated. '''
+   The second part checkes if the inputer inserted is contained in the capital list as a valid capital and returns a sentence with the state associated. '''
 
 
 def check_capital(input):    
 
-    if input in list_of_capitals:    
+    if input in list_of_capitals:  # Checking whether the user input is a valid state in our dictionary   
         print("The capital of {} is {}".format(
             input, list_of_capitals[input]))
     else:
-        for state, capital in list_of_capitals.items():
-            if capital == input:
+        for state, capital in list_of_capitals.items():  
+            if capital == input:  # Checking whether the user input is a valid capital in our dictionary
                  print("The European state whose capital is {} is {}".format(
                      input, state))
                  return
